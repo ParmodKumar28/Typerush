f456674936fc55ed9030d0704964c10ce736919e
Parmod Kumar <yparmodyadav@gmail.com>
2026-06-26 12:04:50 +0530

    sounds logic created

---
+++ src/hooks/useTypingSounds.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import useSound from "use-sound";
+
+export function useTypingSounds(enabled: boolean = true) {
+    const [playKey] = useSound("/sounds/key.mp3", {
+        volume: 0.15,
+        soundEnabled: enabled,
+    });
+
+    const [playWrong] = useSound("/sounds/wrong.mp3", {
+        volume: 0.2,
+        soundEnabled: enabled,
+    });
+
+    const [playComplete] = useSound("/sounds/complete.mp3", {
+        volume: 0.25,
+        soundEnabled: enabled,
+    });
+
+    return {
+        playKey,
+        playWrong,
+        playComplete,
+    };
+}
